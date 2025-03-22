909c85a985884001e62d2293c2bd30258f421e52
viraj <dr.vagulhane@gmail.com>
2025-03-22 11:19:52 +0530

    app.py is addedd

---
+++ app.py
@@ -0,0 +1,20 @@
+n1==int(input('Enter 1st No'))
+n2=int(input('Enter 2nd No'))
+try:
+  div=n1/n2
+  print(div)
+except ZeroDivisionError:
+  print('We cant divide by zero')
+finally:
+  print('Thanks for using our Application')
+  while True:
+    n1=int(input('Enter 1st No'))
+    n2=int(input('Enter 2nd No'))
+    try:
+      div=n1/n2
+      print(div)
+    except ZeroDivisionError:
+      print('We cant divide by zero')
+    finally:
+      print('Thanks for using our Application')
+
